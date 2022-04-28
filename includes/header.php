069cbd21e5fed4b59e3bc23043e7aae21747aaca
Om ghimire <101658218+Om-ghimire1@users.noreply.github.com>
2022-04-28 09:26:13 +0545

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -67,9 +67,9 @@
 						<ul class="nav navbar-nav">
 							<li><a href="index.php">Home</a></li>
 								<li><a href="package-list.php">Tours Package</a></li>
-								<li><a href="Reviews.php">Review</a></li>
-								<!-- <li><a href=""></a></li>
-								<li><a href=""> </a></li> -->
+								<li><a href="resturant.html">Resturant</a></li>
+								 <!-- <li><a href="../resturant.html"></a>Resturant</li> -->
+								<li><a href="hotels.php">hotels </a></li> 
 								<li><a href="coindex.php">Contact Us</a></li>
 								<?php if($_SESSION['login'])
 {?>
